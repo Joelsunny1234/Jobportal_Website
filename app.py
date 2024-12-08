69300c37adc51bed7cbda55c4e9fc8ab854d4a99
Meghna Rajesh <meghucrajesh@gmail.com>
2024-12-08 11:42:39 +0530

    created app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+app.run(debug=True)
